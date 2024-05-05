a2e807c4d122a9ce0a43fcd5dad7984dc893cd41
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-05 22:40:18 +0000

    compute index start && end for page

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import Tuple
+""" module with one function index_range"""
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ compute the index start && end for page"""
+    index_end: int = page * page_size
+    index_start: int = index_end - page_size
+    return (index_start, index_end)
