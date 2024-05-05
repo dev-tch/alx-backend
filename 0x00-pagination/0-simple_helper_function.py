433fd5f626cffa43ca3bcd63e3c5d05a7715d9a3
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-05 22:58:31 +0000

    page index

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from typing import Tuple
 """ module with one function index_range"""
+from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
