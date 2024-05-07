48248cc3af4bcef07102f86443bd86d30e0a2b35
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-07 18:46:21 +0000

    put && get caching data

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache (BaseCaching):
+    def __init__(self):
+        """ constructor method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ add data to caching storage"""
+        valid_data = all((key is not None, item is not None))
+        if valid_data:
+            self.cache_data.update({key: item})
+
+    def get(self, key):
+        """ get data from caching storage """
+        return self.cache_data.get(key, None)
