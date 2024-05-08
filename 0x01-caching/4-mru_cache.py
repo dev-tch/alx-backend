c3cf9340e7623ecf5d9e75952dedf9d143f0b744
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-08 16:19:28 +0000

    mru caching algorithm

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" BaseCaching module with MRU
+"""
+from base_caching import BaseCaching
+from collections import deque
+
+
+class MRUCache (BaseCaching):
+    """
+    implement class
+    """
+    def __init__(self):
+        """ constructor method"""
+        super().__init__()
+        self.deque_container = deque()
+
+    def put(self, key, item):
+        """ add item to caching && handle lifo"""
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            # here we move the key to end (most used )
+            self.deque_container.remove(key)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                old_key = self.deque_container.pop()
+                del self.cache_data[old_key]
+                print("DISCARD: {}".format(old_key))
+        self.cache_data[key] = item
+        self.deque_container.append(key)
+
+    def get(self, key):
+        """ get item from the caching storage"""
+        if (key is None) or (key not in self.cache_data):
+            return None
+        self.deque_container.remove(key)
+        self.deque_container.append(key)
+        return self.cache_data.get(key, None)
